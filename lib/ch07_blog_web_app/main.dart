d7d863b803bfaae4c7e463c8c439159f86aa5d3b
CoitPark <minhobag18@gmail.com>
2023-01-25 17:50:27 +0900

    01-25

---
+++ lib/ch07_blog_web_app/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hello_flutter/ch07_blog_web_app/screen/home_screen.dart';
+
+
+void main(){
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
+}
